5136755faf4291acc18fa3681711bd8b88172243
KeniSam <kenisam@outlook.com.br>
2017-07-09 15:05:45 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,12 +1,31 @@
 import argparse,socket,time, subprocess
 from datetime import datetime
 
-def clear():
-    subprocess.call('clear', shell=True)
+def starting():
+    if (args.port):
+        port()
 
-def hour():
-    now = datetime.now()
-    return "[" + str(now.hour) + ":" + str(now.minute) + "]"
+    else:
+        try:
+            clear()
+            if args.verbose == True:
+                verbose()
+
+            else:
+                escopo()
+                connection()
+
+        except TypeError:
+            print '\033[32m', hour(), ' [INFO] Please try again, you have entered an incorrect ip\n''\033[0;0m\n'
+
+def banner():
+    print '\033[32m'''' 
+  __  ___ .______     ______   .______      .___________.
+ |  |/  / |   _  \   /  __  \  |   _  \     |           |
+ |  '  /  |  |_)  | |  |  |  | |  |_)  |    `---|  |----`
+ |    <   |   ___/  |  |  |  | |      /         |  |     
+ |  .  \  |  |      |  `--'  | |  |\  \----.    |  |     
+ |__|\__\ | _|       \______/  | _| `._____|    |__|     \n''''\033[0;0m'
 
 def escopo():
 
@@ -22,15 +41,6 @@ def escopo():
     print '\033[32m', hour() , " [INFO] Show port's and status:\n"'\033[0;0m'
     time.sleep(1)
 
-def banner():
-    print '\033[32m'''' 
-  __  ___ .______     ______   .______      .___________.
- |  |/  / |   _  \   /  __  \  |   _  \     |           |
- |  '  /  |  |_)  | |  |  |  | |  |_)  |    `---|  |----`
- |    <   |   ___/  |  |  |  | |      /         |  |     
- |  .  \  |  |      |  `--'  | |  |\  \----.    |  |     
- |__|\__\ | _|       \______/  | _| `._____|    |__|     \n''''\033[0;0m'
-
 def connection():
     try:
         for port in port_2:
@@ -41,6 +51,29 @@ def connection():
             if code == 0:
                 print '\033[32m'" Port ", int(port), " Open"'\033[0;0m'
                 time.sleep(0.75)
+        print "\n"
+        exit()
+
+
+    except KeyboardInterrupt:
+        print "\n'\033[32m'" , hour() , " [INFO] The tool was interrupted by the user\n"'\033[0;0m'
+        exit()
+
+def verbose():
+    try:
+        escopo()
+
+        for port in port_2:
+            cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cliente.settimeout(0.15)
+            code = cliente.connect_ex((args.ip, int(port)))
+
+            if code == 0:
+                time.sleep(0.75)
+                print '\033[32m'"  Port ", int(port), " Open"'\033[0;0m'
+            else:
+                time.sleep(0.75)
+                print " Port ", int(port), " Closed"
         print "\n"
         exit()
 
@@ -69,27 +102,12 @@ def port():
         print "\n'\033[32m'" , hour() , " [INFO] The tool was interrupted by the user\n"'\033[0;0m'
         exit()
 
-def verbose():
-    try:
-        escopo()
+def clear():
+    subprocess.call('clear', shell=True)
 
-        for port in port_2:
-            cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            cliente.settimeout(0.15)
-            code = cliente.connect_ex((args.ip, int(port)))
-
-            if code == 0:
-                time.sleep(0.75)
-                print '\033[32m'"  Port ", int(port), " Open"'\033[0;0m'
-            else:
-                time.sleep(0.75)
-                print " Port ", int(port), " Closed"
-        print "\n"
-        exit()
-
-    except KeyboardInterrupt:
-        print "\n'\033[32m'" , hour() , " [INFO] The tool was interrupted by the user\n"'\033[0;0m'
-        exit()
+def hour():
+    now = datetime.now()
+    return "[" + str(now.hour) + ":" + str(now.minute) + "]"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-ip","--ip", dest="ip", help="Enter the Target IP")
@@ -99,18 +117,4 @@ args = parser.parse_args()
 
 port_2 = range(50000)
 
-if (args.port):
-    port()
-
-else:
-    try:
-        clear()
-        if args.verbose == True:
-            verbose()
-
-        else:
-            escopo()
-            connection()
-
-    except TypeError:
-        print '\033[32m' , hour() , ' [INFO] Please try again, you have entered an incorrect ip\n''\033[0;0m\n'
+starting()
